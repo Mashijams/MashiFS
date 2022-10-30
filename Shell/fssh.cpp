6a5f747e48815eb088789842b031dc1c4dd47299
Mashijams <raghavself28@gmail.com>
2022-10-30 17:22:27 +0530

    Changed parameters in FS_SHELL

--- Shell/fssh.cpp
+++ Shell/fssh.cpp
@@ -17,39 +17,41 @@
 
 
 // Command Shell functions
-status_t Mount(Disk &disk, FileSystem &fs, char *arg1);
-status_t Create(Disk &disk, FileSystem &fs, char *arg1, char *arg2);
-void Help();
-void Shell();
+status_t	Mount(Disk &disk, FileSystem &fs, char *DiskName);
+status_t	Create(Disk &disk, FileSystem &fs, char *DiskName, int TotalBlocks);
+void		Shell(FileSystem &fs);
 
 
 // Main execution
 int main (int argc, char* argv[]) {
 	Disk	disk;
-    MashiFS	fs;
+	MashiFS	fs;
 
-	if (argc != 3 && argc != 4) {
+	if (argc != 2 && argc != 3) {
 		fprintf(stderr, "Usage:  <MashiFS> <Mount> <DiskName>\n\n");
-		fprintf(stderr, "Usage:  <MashiFS> <Create> <DiskName> <BlockSize>\n\n");
+		fprintf(stderr, "Usage:  <MashiFS> <Create> <DiskName> <TotalBlocks>\n\n");
     	return EXIT_FAILURE;
 	}
 
 	// If we are creating a new disk with MashiFS
-	if (StringEquals(argv[1], "Create")) {
-		status_t status = Create(disk, fs, argv[2], argv[3]);
-		if (status == F_SUCCESS)
+	if (StringEquals(argv[0], "Create")) {
+		status_t status = Create(disk, fs, argv[1], stoi(argv[2]));
+		if (status == F_SUCCESS) {
+			// if creation and mount is successful run our shell
+			Shell(fs);
 			return EXIT_SUCCESS;
+		}
 
 		// return failure otherwise
 		return EXIT_FAILURE;
 	}
 
 	// If we are mounting existing disk
-	if (StringEquals(argv[1], "Mount")) {
-		status_t status = Mount(disk, fs, argv[2]);
+	if (StringEquals(argv[0], "Mount")) {
+		status_t status = Mount(disk, fs, argv[1]);
 		if (status == F_SUCCESS) {
 			// if mount is successful run our shell
-			Shell();
+			Shell(fs);
 			return EXIT_SUCCESS;
 		}
 
@@ -61,34 +63,47 @@ int main (int argc, char* argv[]) {
 
 
 status_t
-Create(Disk &disk, FileSystem &fs, char *arg1, char *arg2)
+Create(Disk &disk, FileSystem &fs, char *DiskName, size_t TotalBlocks)
 {
-	status_t status = disk.Create(fs, arg1, arg2);
+	status_t status = disk.Create(fs, DiskName, TotalBlocks);
 
 	if (status == F_SUCCESS) {
-		printf("Disk %s created successfully, you can now mount it\n\n", arg1);
+		printf("Disk %s created successfully and mounted with MashiFS\n\n", DiskName);
 	}
 
 	if (status == F_FAIL) {
-		printf("Disk %s creation failed\n\n", arg1);
+		printf("Disk %s creation failed\n\n", DiskName);
 	}
+
+	// Initialise MashiFS on newly created disk
+	status = fs.Init(disk, TotalBlocks);
 
 	return status;
 }
 
 
 status_t
-Mount(Disk &disk, FileSystem &fs, char *arg1)
+Mount(Disk &disk, FileSystem &fs, char *DiskName)
 {
-	status_t status = disk.Mount(fs, arg1);
+	status_t status = disk.Mount(fs, DiskName);
 
 	if (status == F_SUCCESS) {
-		printf("Disk %s mounted successfully\n\n", arg1);
+		printf("Disk %s mounted successfully\n\n", DiskName);
 	}
 
 	if (status == F_FAIL) {
-		printf("Mounting disk %s failed\n\n", arg1);
+		printf("Mounting disk %s failed\n\n", DiskName);
 	}
 
+	// Mount FS from this disk
+	status = fs.Mount(disk);
+
 	return status;
+}
+
+
+void
+Shell(FileSystem &fs)
+{
+	printf("Currently not implemented\n\n");
 }
